2bb9eb96aeca92cde5754cb3128f9bbe00213d00
NotANekomancer <84757707+PhKhang@users.noreply.github.com>
2022-09-17 21:07:57 +0700

    Changes made and committed from VSCode

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SimplePage/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SimplePage from './components/SimplePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SimplePage
+}
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,500;1,600;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,500;1,600;1,700&family=Noto+Sans+SC:wght@100;300;400;500;700;900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'IBM Plex Mono', 'Noto Sans SC', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-weight: 500;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/SimplePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Vue App</title>
+    </head>
+    <body>
+        <p>你好 你很高兴吗？</p>
+    </body>
+    </html>
+</template>
+
+<script>
+    export default {
+      name: 'SimplePage',
+      props: {
+        msg: String
+      }
+    }
+    </script>
